6fc48f091d13da51986c3ed5f732d8be572160ab
kine14 <74851601+zeuxi141@users.noreply.github.com>
2024-09-25 09:11:09 +0700

    Fix link css

--- tailwind-css-ui/tailwind-css-coffee-website/public/main.js
+++ tailwind-css-ui/tailwind-css-coffee-website/public/main.js
@@ -1,32 +1,20 @@
 // const topMenu = document.getElementById('custom-top-menu');
 // const topMenuToggle = document.getElementById('custom-toggle-top-menu-icon');
 
-
-// document.addEventListener('click', (e) => {
-//     console.log(e.target);
-//     if(topMenuToggle.contains(e.target)) {
-//         alert('clicked');
-//         //click to Toggle top menu
-//         topMenu.classList.toggle('hidden');
-//     }else {
-//         //click outside to close top menu
-//     }
-// })
-
 document.addEventListener('DOMContentLoaded', () => {
-    const topMenuToggle = document.querySelector('#custom-toggle-top-menu-icon'); // Đảm bảo đúng selector
-    const topMenu = document.querySelector('#custom-top-menu'); // Đảm bảo đúng selector
+    const topMenuToggle = document.querySelector('#custom-toggle-top-menu-icon');
+    const topMenu = document.querySelector('#custom-top-menu'); 
 
-    if (topMenuToggle && topMenu) { // Kiểm tra nếu cả hai đều không null
+    if (topMenuToggle && topMenu) { 
         document.addEventListener('click', (e) => {
-            console.log(e.target);
             if (topMenuToggle.contains(e.target)) {
-                
-                // Click to Toggle top menu
+                topMenu.classList.toggle('custom-topmenu-expanded');
                 topMenu.classList.toggle('hidden');
-            } else {
-                // Click outside to close top menu
-                topMenu.classList.add('hidden'); // Tắt menu nếu click ngoài
+            } else {                
+                if(topMenu.classList.contains('custom-topmenu-expanded')) {
+                    topMenu.classList.remove('custom-topmenu-expanded');
+                    topMenu.classList.add('hidden');
+                }
             }
         });
     } else {
