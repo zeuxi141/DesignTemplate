d9b1c8579272b1b5d6a85df4588981792cfa166d
kine14 <74851601+zeuxi141@users.noreply.github.com>
2024-09-30 02:04:21 +0700

    coffe-website-tailwindcss completed build coffe mugs and product card

--- tailwind-css-ui/tailwind-css-coffee-website/tailwind.config.js
+++ tailwind-css-ui/tailwind-css-coffee-website/tailwind.config.js
@@ -10,19 +10,24 @@ module.exports = {
         'karla': ['Karla', 'sans-serif'],
       },
       colors: {
-        'light-coffee': '#C89F94',
+        'coffee': {
+          50: '#E8D6D0',
+          200: '#C89F94',
+          400: '#A25F4B',
+          600: '#744838',
+
+        }
       },
       zIndex: {
         '2': '2',
       },
       keyframes: {
-        slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
-        }
+        slideDown: {'0%': { transform: 'translateY(-100%)' },'100%': { transform: 'translateY(0)' },},
+        fadeIn:{ from: {opacity: 0}, to: {opacity: 1}}
       },
       animation: {
         slideDown: 'slideDown 0.5s ease-in-out',
+        fadeIn: 'fadeIn 0.5s ease-in-out'
       }
     },
   },
